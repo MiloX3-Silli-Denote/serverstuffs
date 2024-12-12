1d8dd994fb01ecd769becd028b6642257cccef57
Milo:3 Silli Denote <71358745+MiloX3-Silli-Denote@users.noreply.github.com>
2024-12-12 14:45:09 -0500

    Add files via upload

--- serverstuffs/client.lua
+++ serverstuffs/client.lua
@@ -2,10 +2,13 @@ local Client = {};
 Client.__index = Client;
 
 local NEW_ELEM = string.char(0);
+local REQ_ELEM = string.char(1);
+local CALL_SER = string.char(2);
 
 --* channel 0 : serverstuffs messaging
 --* channel 1 : named element messaging
 --* channel 2 : unnamed element messaging
+--* channel 3 : non element messaging
 
 function Client.new(connectAddress)
     local instance = setmetatable({}, Client);
@@ -14,8 +17,15 @@ function Client.new(connectAddress)
     instance.receiveKey = {}; -- dictionary of what the info bytes mean
     instance.sendKey = {}; -- dictionary of what sending prefixes are sent as
 
+    instance.requests = {};
+
+    instance.globalFuncs = {};
+    instance.defaultRecieve = nil
+
     instance.ssFuncs = {
         [string.char(0)] = Client.setElementKey;
+        [string.char(1)] = Client.receiveElement;
+        [string.char(2)] = Client.callFunction;
     };
 
     if connectAddress then
@@ -23,6 +33,11 @@ function Client.new(connectAddress)
     end
 
     return instance;
+end
+function Client:connect(connectAddress)
+    self.server = self.host:connect(connectAddress, 4);
+    self.receiveKey = {};
+    self.sendKey = {};
 end
 
 function Client:setElementKey(data)
@@ -34,11 +49,43 @@ function Client:setElementKey(data)
     print(name);
     self.sendKey[name] = key;
 end
+function Client:receiveElement(data)
+    local nameLen = string.byte(data, 1,1);
 
-function Client:connect(connectAddress)
-    self.server = self.host:connect(connectAddress, 3);
-    self.receiveKey = {};
-    self.sendKey = {};
+    local name = string.sub(data, 2, nameLen + 1);
+    local elem = string.sub(data, nameLen + 2, -1);
+
+    for i, v in ipairs(self.requests) do
+        if v.name == name then
+            v.callback(elem);
+
+            table.remove(self.requests, i);
+            return;
+        end
+    end
+end
+function Client:callFunction(data)
+    local nameLen = string.byte(data, 1,1);
+    local name = string.sub(data, 2, nameLen + 1);
+
+    local curData = string.sub(data, nameLen + 2, -1);
+    local args = {};
+
+    while curData ~= "" do
+        local len = string.byte(curData, 1,1);
+        table.insert(args, string.sub(curData, 2,len + 1));
+
+        curData = string.sub(curData, len + 2, -1);
+    end
+
+    if self.globalFuncs[name] then
+        self.globalFuncs[name](unpack(args))
+    end
+end
+
+function Client:getElement(name, callback)
+    table.insert(self.requests, {name = name, callback = callback});
+    self.server:send(REQ_ELEM .. name, 0);
 end
 
 function Client:addElement(name)
@@ -62,6 +109,26 @@ function Client:sendElement(name, data)
     self.server:send(self.sendKey[name] .. data, 1);
 end
 
+function Client:sendToServer(data)
+    self.server:send(data, 4);
+end
+
+function Client:callOnServer(name, ...)
+    local args = {...};
+
+    local data = string.char(string.len(name)) .. name;
+
+    for i, v in ipairs(args) do
+        data = data .. string.char(string.len(v)) .. v;
+    end
+
+    self.server:send(CALL_SER .. data, 0);
+end
+
+function Client:addCallback(name, callback)
+    self.globalFuncs[name] = callback;
+end
+
 function Client:update(dt)
     local event = self.host:service();
 
@@ -71,6 +138,10 @@ function Client:update(dt)
                 self.ssFuncs[string.sub(event.data, 1,1)](self, string.sub(event.data, 2,-1));
             elseif event.channel == 1 then -- named element messaging
             elseif event.channel == 2 then -- unnamed element messaging
+            elseif event.channel == 3 then -- non element messaging
+                if self.defaultRecieve then
+                    self.defaultRecieve(event.data);
+                end
             end
         elseif event.type == "connect" then
         elseif event.type == "disconnect" then
--- serverstuffs/server.lua
+++ serverstuffs/server.lua
@@ -8,8 +8,17 @@ function Server.new(bindAddress)
 
     instance.connections = {};
 
+    instance.globalFuncs = {
+        ['calllfunc'] = function(...)
+            print(...);
+        end
+    };
+    instance.defaultRecieve = nil;
+
     instance.ssFuncs = {
         [string.char(0)] = instance.addNewElement;
+        [string.char(1)] = instance.getElement;
+        [string.char(2)] = instance.callFunction;
     };
 
     return instance;
@@ -27,6 +36,30 @@ function Server:addNewElement(peer, name)
     print(name);
     peer:send(string.char(0) .. openName .. name, 0);
 end
+function Server:getElement(peer, name)
+    local elem = self.connections[peer].data[name];
+
+    peer:send(string.char(1) .. string.char(string.len(name)) .. name .. elem, 0);
+end
+function Server:callFunction(peer, data)
+    local nameLen = string.byte(data, 1,1);
+    local name = string.sub(data, 2, nameLen + 1);
+
+    local curData = string.sub(data, nameLen + 2, -1);
+    local args = {};
+
+    while curData ~= "" do
+        local len = string.byte(curData, 1,1);
+        table.insert(args, string.sub(curData, 2,len + 1));
+
+        curData = string.sub(curData, len + 2, -1);
+    end
+
+    print(name);
+    if self.globalFuncs[name] then
+        self.globalFuncs[name](peer, unpack(args))
+    end
+end
 
 function Server:getOpenName(peer)
     for i = 0, 255 do
@@ -42,14 +75,25 @@ function Server:update(dt)
     local event = self.host:service();
 
     while event do
-        print("event");
         if event.type == "receive" then
             if event.channel == 0 then -- serverstuffs messaging
+                print(event.data);
                 self.ssFuncs[string.sub(event.data,1,1)](self, event.peer, string.sub(event.data, 2,-1));
             elseif event.channel == 1 then -- named event messaging
-                print(event.data, 1);
-            elseif event.channel == 2 then -- unamed event messaging
-                print(event.data, 2);
+                local nameKey = string.sub(event.data, 1,2);
+                local setVar = string.sub(event.data, 3,-1);
+
+                self.connections[event.peer].data[self.connections[event.peer].receiveKey[nameKey]] = setVar;
+            elseif event.channel == 2 then -- unnamed event messaging
+                local nameLen = string.byte(event.data, 1,1);
+                local name = string.sub(event.data, 2, nameLen + 1);
+                local setVar = string.sub(event.data, nameLen + 2, -1);
+
+                self.connections[event.peer].data[name] = setVar;
+            elseif event.channel == 3 then -- non element messaging
+                if self.defaultRecieve then
+                    self.defaultRecieve(event.data);
+                end
             end
         elseif event.type == "connect" then
             self.connections[event.peer] = {
